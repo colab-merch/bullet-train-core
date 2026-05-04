b8632dd72986df41f056886789150f1b789bf423
Sam Ritchie <sam.ritchie@outlook.com.au>
2026-05-04 20:28:22 +1000

    misc: return "btn" for first_button_primary

--- bullet_train/app/helpers/account/buttons_helper.rb
+++ bullet_train/app/helpers/account/buttons_helper.rb
@@ -1,12 +1,5 @@
 module Account::ButtonsHelper
   def first_button_primary(context = nil)
-    @global ||= {}
-
-    if !@global[context]
-      @global[context] = true
-      "button"
-    else
-      "button-secondary"
-    end
+    "btn"
   end
 end
